7f4e71acd912949b0c0e7b922f01cfa4aba0214b
cjb1979 <christopher.james.boyd@gmail.com>
2018-08-04 13:44:20 +0100

    Create mk_page.js

---
+++ markov/mk_page.js
@@ -0,0 +1,43 @@
+document.getElementById('btn').addEventListener('click', wrapper, true);
+
+function wrapper() {
+
+  let text = "";
+
+  var JP = document.getElementById("lang_JP").checked;
+  var UK = document.getElementById("lang_UK").checked;
+  var CH = document.getElementById("lang_CH").checked;
+  var DE = document.getElementById("lang_DE").checked;
+
+  if (!(JP || UK || CH || DE)) {
+    text = "HELP ME IM TRAPPED HELP ME IM TRAPPED HELP ME IM TRAPPED HELP ME IM TRAPPED HELP ME IM TRAPPED TRUMPET".toLowerCase();
+  }
+
+
+  text += (JP ? document.getElementById('a').value.toLowerCase() : "");
+  text += (UK ? document.getElementById('b').value.toLowerCase() : "");
+  text += (CH ? document.getElementById('c').value.toLowerCase() : "");
+  text += (DE ? document.getElementById('d').value.toLowerCase() : "");
+
+  power = parseInt(document.getElementById("power_slider").value) / 100;
+
+  var samples = (parseInt(document.getElementById("sample_slider").value) / 100);
+
+  text = text.replace("  ", "").replace(/(\r\n\t|\n|\r\t)/gm, "").replace(/\s\s+/g, ' ');
+
+  var n = parseInt(document.getElementById("ngram_slider").value);
+
+  let settings = {
+    text,
+    n,
+    power,
+    samples: 50,
+    examples: 500,
+    as_array: false
+  };
+
+  let t = markov.get(settings);
+
+  document.getElementById('output').innerHTML = t
+
+}
