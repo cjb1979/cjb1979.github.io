8dac29ef7d733de51271b7f6767b6d197dbd291a
cjb1979 <christopher.james.boyd@gmail.com>
2018-08-15 22:40:59 +0100

    Update set.js

--- mandelbrot/set.js
+++ mandelbrot/set.js
@@ -1,202 +1,157 @@
-function mandelbrot() {
+const mandelbrot = () => {
 
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext('2d');
+  const order = (n) => Math.floor(Math.log(Math.abs(n)) / Math.LN10 + 0.000000001);
 
-var b_cvs = document.createElement('canvas');
-var b_ctx = b_cvs.getContext('2d');
-b_cvs.width = cvs.width;
-b_cvs.height = cvs.height;
+  const float2color = (percentage) => {
+    var c_p_dec = 255 * percentage;
+    var c_p_hex = Number(parseInt(c_p_dec, 10)).toString(16);
+    return "#" + c_p_hex + c_p_hex + c_p_hex;
+  }
 
-/*
-x1 -1.2588676966;
-x2 -1.2587907292
-y1 -0.3711189110
-y2 -0.3710433717
-*/
+  const get_color = (x0, y0, MAX_IT) => {
+    const ESCAPE = 200;
+    let x = 0;
+    let y = 0;
+    let iteration = 0;
 
-
-var w = 600;
-
-var offset_x = -2.5;
-var offset_y = -1.75;
-var width = 3.5;
-var height = 3.5;
-
-var x1 = offset_x,
-  x2 = offset_x + width,
-  y1 = offset_y,
-  y2 = offset_y + height;
-
-
-/*
-x1 = -1.2588676966;
-x2 = -1.2587907292;
-y1 = -0.3711189110;
-y2 = -0.3710433717;
-*/
-  
-
-get_mandelbrot(x1, x2, y1, y2);
-
-//get_mandelbrot(-0.7648822992569296, -0.7648819943929859, -0.08803101236266861, -0.08803070749872476, 8065);
-
-function clear_canvas() {
-  b_ctx.save();
-
-  // Use the identity matrix while clearing the canvas
-  b_ctx.setTransform(1, 0, 0, 1, 0, 0);
-  b_ctx.clearRect(0, 0, b_cvs.width, b_cvs.height);
-
-  // Restore the transform
-  b_ctx.restore();
-  
- // https://stackoverflow.com/questions/2142535/how-to-clear-the-canvas-for-redrawing
-
-}
-
-function get_iterations(x1, x2){
-  return Math.floor(Math.log(((5 / Math.abs(x1 - x2)) * 500)) * 200, 0);
-}
-
-function get_mandelbrot(_x1, _x2, _y1, _y2, _max_it) {
-  clear_canvas();
-  let x1 = _x1;
-  let x2 = _x2;
-  let y1 = _y1;
-  let y2 = _y2;
-  
-  
-  let max_it = (_max_it || get_iterations(x1, x2))
-  var small_amount = (x2 - x1) / w;
-  var py = 0;
-  var total = (y1 - y2) * (x1 - x2);
-  
-  for (var y = y1; y <= y2; y += small_amount) {
-    var px = 0;
-    for (var x = x1; x <= x2; x += small_amount) {
-      b_ctx.fillStyle = get_color(x, y, max_it);
-      b_ctx.fillRect(px, py, 1, 1);
-      px++;
+    while (x * x + y * y <= ESCAPE && iteration < MAX_IT) {
+      let xtemp = x * x - y * y + x0;
+      y = 2 * x * y + y0;
+      x = xtemp;
+      iteration++;
     }
-    ctx.fillStyle = "#FF0000";
-    let fill_amt = ((y * x) / total) * w;
-    ctx.fillRect(0, w - 116, fill_amt, 10);
-    //if (py % 100 == 0) ctx.fillRect(0, w - 116, py, 10);
-    py++;
+    
+    if (iteration === MAX_IT) return "#000000" //black
+    var c = Math.log(iteration) / Math.log(MAX_IT - 1.0);
+    return float2color(c);
   }
-  b_ctx.fillStyle = 'red';
-  b_ctx.font = '14px sans-serif';
-  b_ctx.fillText("x1 " + x1.toFixed(5), 1, 15);
-  b_ctx.fillText("x2 " + x2.toFixed(5), 1, 30);
+
+  var cvs = document.getElementById("canvas");
+  var ctx = cvs.getContext('2d');
+
+  var b_cvs = document.createElement('canvas');
+  var b_ctx = b_cvs.getContext('2d');
   
-  b_ctx.fillText("w " + (x2 - x1).toFixed(5), 100, 22);
-  
-  b_ctx.fillText("y1 " + y1.toFixed(5), 1, 45);
-  b_ctx.fillText("y2 " + y2.toFixed(5), 1, 60);
-  
-  
-  b_ctx.fillText("h " + (y2 - y1).toFixed(5), 100, 52);
+  let cursor_move_amt = 0;
 
+  b_cvs.width = cvs.width;
+  b_cvs.height = cvs.height;
 
- b_ctx.fillText("mx " + get_iterations(x1, x2).toFixed(5), 1, 85);
-
-  ctx.drawImage(b_cvs, 0, 0);
-}
-
-function get_color(x0, y0, MAX_IT) {
-  var ESCAPE = 2 * 2,
-    x = 0,
-    y = 0,
-    iteration = 0;
-
-  while (x * x + y * y <= ESCAPE && iteration < MAX_IT) {
-    var xtemp = x * x - y * y + x0;
-    y = 2 * x * y + y0;
-    x = xtemp;
-    iteration++;
+  const w = 600;
+  let rect = {
+    startX: 0,
+    startY: 0,
+    w: w,
+    h: w
   }
-  if (iteration === MAX_IT) return "#000000" //black
-  var c = Math.log(iteration) / Math.log(MAX_IT - 1.0);
-  return float2color(c);
-}
+  let drag = false;
 
+  var offset_x = -2.5;
+  var offset_y = -1.75;
+  var width = 3.5;
+  var height = 3.5;
 
-function float2color(percentage) {
-  var c_p_dec = 255 * percentage;
-  var c_p_hex = Number(parseInt(c_p_dec, 10)).toString(16);
-  return "#" + c_p_hex + c_p_hex + c_p_hex;
-}
+  let x1 = offset_x,
+    x2 = offset_x + width,
+    y1 = offset_y,
+    y2 = offset_y + height;
 
-//https://rosettacode.org/wiki/Mandelbrot_set#JavaScript
+  const get_iterations = (x1, x2) => {
+    let o = Math.abs(order(x2 - x1)) + 2;
+    return 500 * (o * 2);
+  }
 
-var rect = {},
-  drag = false;
+  const get_mandelbrot = (x1, x2, y1, y2, _max_it) => {
 
-function draw() {
-  ctx.drawImage(b_cvs, 0, 0);
-  ctx.setLineDash([6]);
-  ctx.strokeStyle = "#FF0000";
-  ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
-}
+    let max_it = (_max_it || get_iterations(x1, x2));
+    let small_amount = (x2 - x1) / w;
+    let total = (y1 - y2) * (x1 - x2);
+    let scale_str = order(x2 - x1);
 
-function mouseDown(e) {
-  /*
-  rect.startX = e.pageX - this.offsetLeft;
-  rect.startY = e.pageY - this.offsetTop; 
-  */
-  
-  rect.startX = e.offsetX;
-  rect.startY = e.offsetY;
-  
-  drag = true;
-}
+    for (var y = y1, py = 0; y <= y2; y += small_amount, py++) {
+      for (var x = x1, px = 0; x <= x2; x += small_amount, px++) {
+        b_ctx.fillStyle = get_color(x, y, max_it);
+        b_ctx.fillRect(px, py, 1, 1);
+      }
+    }
 
-function mouseUp() {
-  drag = false;
-  x1 = ((rect.startX / w) * width) + offset_x;
-  y1 = ((rect.startY / w) * height) + offset_y;
-  x2 = (((rect.startX + rect.w) / w) * width) + offset_x;
-  y2 = (((rect.startY + rect.h) / w) * width) + offset_y;
-  offset_x = x1;
-  offset_y = y1;
-  width = x2 - x1;
-  height = y2 - y1;
-  var max_it = get_iterations(x1, x2);
+    b_ctx.fillStyle = 'red';
+    b_ctx.font = "15px Verdana";
+    let str = `  Iterations: ${get_iterations(x1, x2)}. Scale: ${scale_str}x`;
+    b_ctx.fillText(str, 1, 20);
+    ctx.drawImage(b_cvs, 0, 0);
+  }
 
-  get_mandelbrot(x1, x2, y1, y2, max_it);
-  
-  console.log({
-  x1,
-  x2,
-  y1,
-  y2,
-  max_it
-  });
-  
-}
+  //https://rosettacode.org/wiki/Mandelbrot_set#JavaScript
 
-function mouseMove(e) {
-  if (drag) {
+  const draw = () => {
+    ctx.drawImage(b_cvs, 0, 0);
+    ctx.setLineDash([6]);
+    ctx.strokeStyle = "#FF0000";
+    ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
+  }
+
+  const mouseDown = (e) => {
+    rect.startX = e.offsetX;
+    rect.startY = e.offsetY;
+    drag = true;
+  }
+
+  const mouseUp = () => {
+    if (!cursor_move_amt) return
+    drag = false;
+    cursor_move_amt = 0;
+    let t_x1 = ((rect.startX / w) * width) + offset_x;
+    let t_y1 = ((rect.startY / w) * height) + offset_y;
+    let t_x2 = (((rect.startX + rect.w) / w) * width) + offset_x;
+    let t_y2 = (((rect.startY + rect.h) / w) * width) + offset_y;
+    
+    x1 = Math.min(t_x1, t_x2);
+    x2 = Math.max(t_x1, t_x2);
+    
+    y1 = Math.min(t_y1, t_y2);
+    y2 = Math.max(t_y1, t_y2);
+    
+    offset_x = x1;
+    offset_y = y1;
+    width = x2 - x1;
+    height = y2 - y1;
+    
+    let max_it = get_iterations(x1, x2);
+
+    get_mandelbrot(x1, x2, y1, y2, max_it);
+
+    console.log({
+      x1,
+      x2,
+      y1,
+      y2,
+      max_it
+    });
+
+  }
+
+  const mouseMove = (e) => {
+    if (!drag) return;
+    cursor_move_amt++;
     let ratio = canvas.width / canvas.height;
-    /*
-    rect.w = (e.pageX - this.offsetLeft) - rect.startX;
-    */
-    rect.w =  e.offsetX - rect.startX;
-
-    rect.h =  rect.w * ratio;
-    //(e.pageY - this.offsetTop) - rect.startY;
+    if (e.offsetX > e.offsetY) {
+      rect.w = e.offsetX - rect.startX;
+      rect.h = rect.w * ratio;
+    } else {
+      rect.h = e.offsetY - rect.startY;
+      rect.w = rect.h * ratio;
+    }
     draw();
   }
 
-}
+  const init = () => {
+    cvs.addEventListener('mousedown', mouseDown, false);
+    cvs.addEventListener('mouseup', mouseUp, false);
+    cvs.addEventListener('mousemove', mouseMove, false);
+  }
 
-function init() {
-  cvs.addEventListener('mousedown', mouseDown, false);
-  cvs.addEventListener('mouseup', mouseUp, false);
-  cvs.addEventListener('mousemove', mouseMove, false);
-}
-
-init();
+  init();
+  get_mandelbrot(x1, x2, y1, y2);
 
 };
