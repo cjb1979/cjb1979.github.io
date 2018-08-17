d8e6d5ee54bddf3fb85c3f8a3d282d346e388953
cjb1979 <christopher.james.boyd@gmail.com>
2018-08-17 14:02:55 +0100

    Create breadcrumb.js

---
+++ global/breadcrumb.js
@@ -0,0 +1,29 @@
+const get_breadcrumb = (() => {
+
+  let path = window.location.pathname;
+
+  let arr = path.split("/");
+  arr[0] = "Home";
+  arr = arr.filter(x => x);
+
+  const breadcrumb = (arr) => {
+
+    let html_arr = arr.map((x, i) => {
+      let link_text, link_url, str;
+      if (x === "Home") {
+        link_text = x;
+        link_url = "/";
+      } else {
+        link_text = x;
+        link_url = x;
+      }
+
+      return i === arr.length - 1 ? link_text : `<a href = "${link_url}">${link_text}</a>`;
+    });
+
+    let str = html_arr.join(" > ");
+
+    return str;
+  }
+  return breadcrumb(arr);
+})();
