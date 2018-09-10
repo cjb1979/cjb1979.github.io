1645cd300d0abbd1a1909adaf6ef92ee1b4e3206
cjb1979 <christopher.james.boyd@gmail.com>
2018-09-10 20:23:01 +0100

    Update breadcrumb.js

--- global/breadcrumb.js
+++ global/breadcrumb.js
@@ -9,7 +9,7 @@ const breadcrumb = ((path = window.location.pathname) => {
     const upper = (text) => text.replace(/^\w/, c => c.toUpperCase());
     const html_arr = arr.map((x, i, arr) => {
       const link_text = upper(x);
-      const link_url = i === 0 ? "/" : arr.slice(1, i + 1).join("/");
+      const link_url = "/" +  arr.slice(1, i + 1).join("/");
       return i === arr.length - 1 ? link_text : `<a href = "${link_url}">${link_text}</a>`;
     });
     return html_arr.join(" / ");
